a2e67fbcbc4e0ad7fee111e3a85475acb3241332
Thais Palma de Brito <britop.tech@gmail.com>
2025-12-14 00:11:50 -0800

    update .gitignore and create Volunteer model

---
+++ models/Volunteer.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+
+const volunteerSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    firstName: {
+        type: String,
+        required: true,    
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    cause: {
+        type: String,
+        enum: ["A", "B", "C", "D"],  //Update fields later
+        required: true,
+    },
+    skills: {
+        type: String,
+        required: true,
+    },
+    model: {
+        num: ["In-Person", "Remote"],
+        required: true,
+
+    },
+    availability: {
+        type: String,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    contactInfo: {
+        type: String,
+        required: true,
+    },
+    bio: {
+        type: String,
+        required: true,
+    },
+    comments: {
+        type: String,
+    },
+});
+
+const Volunteer = mongoose.model("Volunteer", volunteerSchema);
+
+module.exports = Volunteer;
