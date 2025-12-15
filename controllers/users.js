14a3e522d9dd6f223fcdeeea31b2cc44964e2f30
Thais Palma de Brito <britop.tech@gmail.com>
2025-12-15 13:11:32 -0800

    add users controller and homepage route; add volunteer profile route and update server.js

---
+++ controllers/users.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+const Volunteer = require("../models/Volunteer");
+
+router.get("/", async (req, res, next) => {
+  try {
+    let volunteer = null;
+    const user = req.session.user;
+    if (user) {
+      volunteer = await Volunteer.findOne({ userId: user._id });
+    }
+    res.render('index.ejs', { user, volunteer });
+
+  } catch (err) {
+    console.log(err);
+    res.redirect("/")
+  }
+});
+
+module.exports = router;
--- controllers/volunteers.js
+++ controllers/volunteers.js
@@ -3,4 +3,24 @@ const router = express.Router()
 
 const Volunteer = require('../models/Volunteer')
 
+// GET volunteer/profile
+router.get('/profile', async (req, res) => {
+    try {
+
+        const user = req.session.user;
+        if (!user) return res.redirect("/auth/sign-in")
+
+        const volunteer = await Volunteer.findOne({userId: user._id});
+        if (!volunteer) {
+            return res.redirect('/volunteer/new');
+        }
+
+        res.render('volunteers/show.ejs', { volunteer })
+
+    } catch (err) {
+        console.log(err)
+        res.redirect('/')
+    }
+})
+
 module.exports = router;
